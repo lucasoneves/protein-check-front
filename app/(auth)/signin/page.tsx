47cffbc5c290191f348e8c6ea021e02633375bd1
lucasoneves <casluhc@gmail.com>
2023-07-17 22:30:20 -0300

    Validate username and userpassword

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -6,18 +6,34 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function SignInPage() {
-  const [dataLogin, setDataLogin] = useState({ email: '', password: ''})
-  function handleSignIn(event) {
-    event.preventDefault();
-    console.log("handleSignIn", dataLogin);
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [error, setError] = useState([]);
+
+  function saveUserEmail(e) {
+    setUserEmail(e.target.value);
   }
 
-  function handleChangeField(event, field) {
-    const value = event.target.value
-    const formField = field
-    setDataLogin((prevState) => {
-      return {...prevState, formField: event.target.value}
-    });
+  function saveUserPassword(e) {
+    setUserPassword(e.target.value);
+  }
+
+  function handleValidateEmail() {
+    if (!userEmail) {
+      setError((prevState) => [...prevState, { error: "previous state"}])
+    }
+  }
+
+  function handleValidatePassword() {
+    if (!userPassword) {
+      alert("por favor, preecha o campo senha");
+    }
+  }
+
+  function submitLoginForm(e) {
+    e.preventDefault();
+    handleValidateEmail();
+    handleValidatePassword();
   }
 
   return (
@@ -26,19 +42,34 @@ export default function SignInPage() {
         <h2>Olá!</h2>
         <p>Entre com seu email e senha para acessar a plataforma</p>
       </header>
-      <form action="" onSubmit={handleSignIn}>
+      <form action="" onSubmit={submitLoginForm}>
         <label htmlFor="email">
           Email
-          <input className={inputStyle['input']} type="email" value={dataLogin.email} onChange={(e) => handleChangeField('email')} />
+          <input
+            className={inputStyle["input"]}
+            type="email"
+            value={userEmail}
+            onChange={saveUserEmail}
+          />
         </label>
         <label htmlFor="password">
           Senha
-          <input className={inputStyle['input']} type="password" value={dataLogin.password} onChange={(e) => handleChangeField(e, 'password')} />
+          <input
+            className={inputStyle["input"]}
+            type="password"
+            value={userPassword}
+            onChange={saveUserPassword}
+          />
         </label>
-        <Link className={styles['link']} href="/recoverpassword">Esqueci minha senha</Link>
+        <pre>{error}</pre>
+        <Link className={styles["link"]} href="/recoverpassword">
+          Esqueci minha senha
+        </Link>
         <Button>Entrar</Button>
       </form>
-      <p className={styles['signup-link']}>Não tem uma conta? <Link href="/signup">Cadastre-se</Link></p>
+      <p className={styles["signup-link"]}>
+        Não tem uma conta? <Link href="/signup">Cadastre-se</Link>
+      </p>
     </div>
   );
 }
