d1aa584d1380b1bf4aacd10508b9a3be129a9fab
lucasoneves <casluhc@gmail.com>
2023-08-04 21:41:54 -0300

    Header component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <main>
-      <h2>Protein check</h2>
+      
     </main>
   )
 }
---
+++ components/Card/index.tsx
@@ -0,0 +1,5 @@
+import styles from './Card.module.scss';
+
+export const Card = () => {
+  return <div className={`${styles['card']} rounded-lg p-2`}>card 1</div>
+}
