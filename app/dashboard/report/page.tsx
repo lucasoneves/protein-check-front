71ecccac311c40509aabd911d4da5ff73ccaad50
lucasoneves <casluhc@gmail.com>
2023-08-28 20:43:24 -0300

    WIP: Edit item feature (home)

--- app/dashboard/report/page.tsx
+++ app/dashboard/report/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 function Report() {
   return <><div className="mt-5"><h2>Report</h2></div></>
--- components/CardDaily/index.tsx
+++ components/CardDaily/index.tsx
@@ -4,13 +4,11 @@ import { VscEdit, VscTrash } from "react-icons/vsc";
 
 function CardDaily({
   createdAt,
-  children,
   amount,
   handleEdit,
   handleDelete,
   id
 }: {
-  children: ReactNode,
   createdAt: string;
   amount: number;
   handleEdit: ReactEventHandler<Element>;
@@ -21,7 +19,7 @@ function CardDaily({
     <Card className="card-amount">
       <div className={`p-3 flex gap-6 rounded-md text-sm`}>
         <span>{createdAt}</span>
-        {children}
+        <span className="flex-1">{amount}g</span>
         <div className="actions flex gap-4 text-xl">
           <button onClick={handleEdit} className="flex gap-1"> <VscEdit /> </button>
           <button onClick={handleDelete} className="flex gap-1"> <VscTrash /> </button>
