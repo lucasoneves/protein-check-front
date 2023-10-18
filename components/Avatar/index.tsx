3f6edf1f61b7c589f511c551d0fdc40df1447ce7
lucasoneves <casluhc@gmail.com>
2023-10-18 19:31:27 -0300

    WIP: Logout handler

---
+++ components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { MouseEventHandler, ReactNode } from "react";
+type UserHeader = {
+  userName: string;
+  children: ReactNode;
+  handleClick: MouseEventHandler
+};
+export default function Avatar({ userName, children, handleClick }: UserHeader) {
+  return (
+    <div className="avatar flex items-center gap-4 relative">
+      <h2 className="text-xs sm:text-sm">{userName && `Hello, ${userName}`}</h2>
+      <div onClick={handleClick} className="bg-slate-500 font-bold w-8 h-8 flex items-center justify-center rounded-full text-sm">
+        {userName &&
+          userName.split(" ")[0][0].concat(userName.split(" ")[1][0])}
+      </div>
+      {children}
+    </div>
+  );
+}
