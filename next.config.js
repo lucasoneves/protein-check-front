4154c2dd6a4ee931bb2199f55c9c94787527f884
lucasoneves <casluhc@gmail.com>
2023-10-29 10:27:46 -0300

    Setting up a index page to signin

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/signin',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
